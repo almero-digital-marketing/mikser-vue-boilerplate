8e333554b80b2920cf563aea0a24e3d3bb4677fa
dimitarkolev <dk@almero.bg>
2021-07-26 09:38:04 +0000

    Woo Prefix

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,7 +2,7 @@
 	<div class="product">
 		<h1>{{ document.meta.title }} / {{ woo.slug }}</h1>
 		<ul>
-			<li v-for="item in products" :key="item.permalink">{{ item.name }}: {{ item.price }} <button @click="addToCart(item.id)">Add to cart</button></li>
+			<li v-for="item in wooProducts" :key="item.permalink">{{ item.name }}: {{ item.price }} <button @click="addToCart(item.id)">Add to cart</button></li>
 		</ul>
 	</div>
 </template>
